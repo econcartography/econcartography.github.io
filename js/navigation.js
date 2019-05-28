7120cfd8fd394cc2e1ce0d7a8eb869ad2cd5c71d
musci8 <federico.musciotto@unipa.it>
2019-05-28 17:26:47 +0200

    rebuilding site Tue 28 May 2019 05:26:47 PM CEST

---
+++ js/navigation.js
@@ -0,0 +1,29 @@
+// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+
+
+
+// on scroll, 
+$(document).ready(function(){
+  $(window).on('scroll',function(){
+    // we round here to reduce a little workload
+    var exploretop = $('#explore').offset().top-25;
+    var blogtop = $('#blog').offset().top-25;
+    var gittop = $('#contact-us').offset().top-90;
+    stop = Math.round($(window).scrollTop());
+    if (stop > exploretop) {
+      $('.navigation').addClass('explore-color');
+    } else {
+      $('.navigation').removeClass('explore-color');
+    }
+    if (stop > blogtop) {
+      $('.navigation').addClass('blog-color');
+    } else {
+      $('.navigation').removeClass('blog-color');
+    }
+    if (stop > gittop) {
+      $('.navigation').addClass('git-color');
+    } else {
+      $('.navigation').removeClass('git-color');
+    }
+  });
+});
